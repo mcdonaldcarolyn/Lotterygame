85ef7a290a582681f62183b5e0cf6f548f102946
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-10 18:24:43 +0000

    create ininput for player to guess numbers

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import random
 
 print('Powerbal Lottery by inventwithpython')
 
+While True:
+    print('Enter 4 different numbers from 1 to 89, with space between')
+    print('each number. (for example: 5 17 23 42 50)')
+    response = input('> ')
+
+    
